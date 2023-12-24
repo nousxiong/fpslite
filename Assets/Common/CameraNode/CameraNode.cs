23d32ffaf2a40df3b2221cf5bb95c9727e2a3d25
nousxiong <348944179@qq.com>
2023-12-24 23:25:35 +0800

    cameranode连线编辑开发中

---
+++ Assets/Common/CameraNode/CameraNode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Common.CameraNode
+{
+    public class CameraNode : MonoBehaviour
+    {
+        public Color gizmoColor = Color.grey;
+
+        private void OnDrawGizmos()
+        {
+            DrawBox();
+        }
+
+        private void DrawBox()
+        {
+            if (!gameObject.activeInHierarchy)
+            {
+                return;
+            }
+            // 保存原始Gizmos矩阵
+            Matrix4x4 originalMatrix = Gizmos.matrix;
+            Gizmos.color = gizmoColor;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            Gizmos.matrix = originalMatrix;
+        }
+    }
+}
---
+++ Assets/Common/Editor/CameraNodeEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Common.Editor
+{
+    [CustomEditor(typeof(CameraNode.CameraNode))]
+    public class CameraNodeEditor : UnityEditor.Editor
+    {
+        private CameraNode.CameraNode currentNode;
+
+        private void OnSceneGUI()
+        {
+            Event e = Event.current;
+            var cameraNode = (CameraNode.CameraNode)target;
+            Debug.Log($"CameraNode {cameraNode.transform.position} {e.type} OnSceneGUI");
+            // 处理鼠标点击事件
+            if (e.type == EventType.MouseUp && e.button == 0)
+            {
+                Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                Debug.Log($"MouseUp {ray.origin}");
+
+                if (Physics.Raycast(ray, out var hit))
+                {
+                    CameraNode.CameraNode hitNode = hit.collider.GetComponent<CameraNode.CameraNode>();
+                    if (hitNode != null)
+                    {
+                        if (currentNode == null)
+                        {
+                            // 第一次点击
+                            currentNode = hitNode;
+                            Handles.DrawLine(currentNode.transform.position, ray.origin);
+                        }
+                        else if (hitNode != currentNode)
+                        {
+                            // 第二次点击，绘制线并重置
+                            Debug.DrawLine(currentNode.transform.position, 
+                                hitNode.transform.position, Color.red);
+                            currentNode = null;
+                        }
+                    }
+                    else
+                    {
+                        currentNode = null;
+                    }
+                }
+                else
+                {
+                    currentNode = null;
+                }
+                HandleUtility.Repaint();
+            }
+        }
+    }
+}

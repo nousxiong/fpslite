7d9f0bb208dbf49b2a2802a518d0664308c2023c
nousxiong <348944179@qq.com>
2023-12-25 22:57:27 +0800

    测试Editor绘制辅助线，结论：换方式

--- Assets/Common/Editor/CameraNodeEditor.cs
+++ Assets/Common/Editor/CameraNodeEditor.cs
@@ -6,49 +6,34 @@ namespace Common.Editor
     [CustomEditor(typeof(CameraNode.CameraNode))]
     public class CameraNodeEditor : UnityEditor.Editor
     {
-        private CameraNode.CameraNode currentNode;
+        private CameraNode.CameraNode startNode;
 
         private void OnSceneGUI()
         {
             Event e = Event.current;
             var cameraNode = (CameraNode.CameraNode)target;
             Debug.Log($"CameraNode {cameraNode.transform.position} {e.type} OnSceneGUI");
-            // 处理鼠标点击事件
-            if (e.type == EventType.MouseUp && e.button == 0)
+            // if (e.type == EventType.MouseMove)
             {
-                Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-                Debug.Log($"MouseUp {ray.origin}");
-
-                if (Physics.Raycast(ray, out var hit))
+                if (startNode == null)
                 {
-                    CameraNode.CameraNode hitNode = hit.collider.GetComponent<CameraNode.CameraNode>();
-                    if (hitNode != null)
-                    {
-                        if (currentNode == null)
-                        {
-                            // 第一次点击
-                            currentNode = hitNode;
-                            Handles.DrawLine(currentNode.transform.position, ray.origin);
-                        }
-                        else if (hitNode != currentNode)
-                        {
-                            // 第二次点击，绘制线并重置
-                            Debug.DrawLine(currentNode.transform.position, 
-                                hitNode.transform.position, Color.red);
-                            currentNode = null;
-                        }
-                    }
-                    else
-                    {
-                        currentNode = null;
-                    }
+                    startNode = cameraNode;
                 }
-                else
+                
+                if (startNode == cameraNode)
                 {
-                    currentNode = null;
+                    Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    // Debug.Log($"mouse ray {ray.origin}");
+                    Handles.color = Color.yellow;
+                    Handles.DrawLine(startNode.transform.position, ray.origin);
+                    // HandleUtility.Repaint();
                 }
-                HandleUtility.Repaint();
+                else if (startNode != null)
+                {
+                    Handles.DrawLine(startNode.transform.position, cameraNode.transform.position);
+                }
             }
+            HandleUtility.Repaint();
         }
     }
 }

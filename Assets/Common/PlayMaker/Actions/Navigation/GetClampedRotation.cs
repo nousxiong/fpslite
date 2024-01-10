e192db6c7c43e8e74b930d684f44eb4b27e66918
熊小磊 <xiongxiaolei@lightboat.games>
2024-01-10 21:12:24 +0800

    GetClampedRotation对比新旧Paths

--- Assets/Common/PlayMaker/Actions/Navigation/GetClampedRotation.cs
+++ Assets/Common/PlayMaker/Actions/Navigation/GetClampedRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 // ReSharper disable CheckNamespace
@@ -23,10 +24,41 @@ namespace HutongGames.PlayMaker.Actions
         /// 上一次的paths
         /// </summary>
         List<GameObject> prevPaths = new List<GameObject>();
+        /// <summary>
+        /// 上一次到当前新加入的Paths
+        /// </summary>
+        List<GameObject> addedPaths = new List<GameObject>();
+        /// <summary>
+        /// 上一次到当前移除的Paths
+        /// </summary>
+        List<GameObject> removedPaths = new List<GameObject>();
+        /// <summary>
+        /// 上一次到当前保留的Paths
+        /// </summary>
+        List<GameObject> remainPaths = new List<GameObject>();
+        /// <summary>
+        /// 当前的Path，用于旋转基点计算
+        /// </summary>
+        GameObject currentPath;
 
         public override void Reset()
         {
             paths = null;
+            prevPaths.Clear();
+            addedPaths.Clear();
+            removedPaths.Clear();
+            remainPaths.Clear();
+            currentPath = null;
+        }
+
+        public override void Init(FsmState state)
+        {
+            base.Init(state);
+            prevPaths.Clear();
+            addedPaths.Clear();
+            removedPaths.Clear();
+            remainPaths.Clear();
+            currentPath = null;
         }
 
 		// Code that runs on entering the state.
@@ -47,14 +79,67 @@ namespace HutongGames.PlayMaker.Actions
             {
                 return;
             }
+            
+            var prevCount = prevPaths.Count;
+            var newCount = paths.Length;
+            var prevEmpty = prevCount == 0;
+            var newEmpty = newCount == 0;
 
-            if (paths.Length == 0)
+            remainPaths.Clear();
+            addedPaths.Clear();
+            removedPaths.Clear();
+            
+            // 找出和prevPaths不同的地方
+            for (var i = 0; i < newCount; i++)
             {
+                var path = paths.Get(i) as GameObject;
+                if (prevPaths.Contains(path))
+                {
+                    // remain
+                    remainPaths.Add(path);
+                }
+                else
+                {
+                    // added
+                    addedPaths.Add(path);
+                }
+            }
+
+            foreach (GameObject prevPath in prevPaths)
+            {
+                var id = -1;
+                id = Array.IndexOf(paths.Values, prevPath);
+                var contained = id != -1;
+                if (!contained)
+                {
+                    // removed
+                    removedPaths.Add(prevPath);
+                }
+            }
+            
+            var addedCount = addedPaths.Count;
+            var removedCount = removedPaths.Count;
+            var remainCount = remainPaths.Count;
+            
+            // save new paths
+            prevPaths.Clear();
+            for (var i = 0; i < newCount; i++)
+            {
+                var path = paths.Get(i) as GameObject;
+                prevPaths.Add(path);
+            }
+
+            if (addedCount == 0 && removedCount == 0 && remainCount == prevCount && currentPath != null)
+            {
+                // no change
                 return;
             }
             
-            // 遍历paths，计算最大夹角的两个path
-            
+            // 决定当前的Path
+            var selfPath = currentPath == go;
+            if (remainCount > 0)
+            {
+            }
         }
 
 
---
+++ Assets/Common/Utiliy/Vector3Extensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Common.Utiliy
+{
+    public static class Vector3Extensions
+    {
+        public static Vector3 Abs(this Vector3 vector)
+        {
+            return new Vector3(Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z));
+        }
+
+        public static Vector3 ProjectionXZ(this Vector3 vector)
+        {
+            return new Vector3(vector.x, 0, vector.z);
+        }
+
+        public static Vector2 ProjectionXZVector2(this Vector3 vector)
+        {
+            return new Vector2(vector.x, vector.z);
+        }
+    }
+}
